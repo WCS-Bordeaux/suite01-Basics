1d409e63a485a72ef5dc5db1317912ae1fd4ff21
Massimiliano <mozz76@gmail.com>
2017-03-27 16:29:07 +0200

    Function that returns the sum of two numbers declared and called twice with two differrent pairs of input values.

--- ex03.js
+++ ex03.js
@@ -29,3 +29,12 @@
 
 //  écrire votre code sous ce commentaire
 
+function addition(a, b) {
+    return a + b;
+}
+
+// Test 1 Result
+console.log(addition(6, 9));
+
+// Test 2 Result
+console.log(addition(98766, 345678));
