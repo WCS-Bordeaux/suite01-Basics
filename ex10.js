e7eaa7ae15eee52394c8574fc0981cc8ed0e0fbd
facolombani <facolombani@gmail.com>
2017-03-28 11:49:06 +0200

    exercice 10 fait

--- ex10.js
+++ ex10.js
@@ -18,3 +18,22 @@
 
 */
 
+var getPerson = {
+    fullName :{
+        firstName : "toto",
+        lastName : "tata",
+    },
+    age : 45,
+}
+
+function isAdult (firstName) {
+    if ( getPerson.age >= 124 || getPerson.age < 1) {
+        return "impossible... ou encore jamais vu";
+    } else if (getPerson.age >= 12 && getPerson.age < 18) {
+        return "ado";
+    } else if (getPerson.age >= 18 && getPerson.age < 124) {
+        return "majeur";
+    } else if (getPerson.age < 12){
+        return "enfant";
+    }
+}
