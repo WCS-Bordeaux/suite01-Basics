2f66c615af02c0c068a3364bff9c7162884d355e
fahemuu <fahem.id@gmail.com>
2017-03-28 09:16:37 +0200

    Excercice_08 fonction qui detecte s'il existe un nombre pair dans un tableau

--- ex08.js
+++ ex08.js
@@ -30,4 +30,21 @@
 // isAllEven([4, 4, 6, 8, 10, 12]);
 
 //  écrire votre code sous ce commentaire
+function isEven(nb) {
+    if (nb % 2 === 0) {
+        return 1;
+    }
+    else
+        return 0;
+}
+//___________________________________
 
+function isAllEven(tab) {
+
+    for (i = 0; i < tab.length; i++) {
+        if (isEven(tab[i]) === 0) {
+            return 1;
+        }
+    }
+    return 0;
+}
