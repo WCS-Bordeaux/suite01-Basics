b2bf9fe9f88ada36a9721908eab3b32fc6051dea
Massimiliano <mozz76@gmail.com>
2017-03-28 20:07:31 +0200

    Any call to the functions was removed

--- ex01.js
+++ ex01.js
@@ -18,14 +18,18 @@
     qui retourne ce paramètre "nb";
 */
 
+
+
+//  écrire votre code sous ce commentaire
+
 function returnParameter(nb) {
     return nb;
 }
 
-//  écrire votre code sous ce commentaire
-
+/*
 console.log(returnParameter(5));
 console.log(returnParameter("Massimiliano"));
 console.log(returnParameter(true));
 console.log(returnParameter([13, 17]));
 console.log(returnParameter({key1: 5, key2: "Massi", key3: false}));
+*/
